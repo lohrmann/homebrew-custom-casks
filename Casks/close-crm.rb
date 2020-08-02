58fe51cf5fd703f75247b4221a81b667602f5fbb
Daniel Lohrman <d@lhr.mn>
2020-08-02 17:01:35 +1000

    Update close-crm.rb

--- Casks/close-crm.rb
+++ Casks/close-crm.rb
@@ -1,15 +1,14 @@
 cask "close-crm" do
-    version "5.0.0"
-    sha256 "0b561615382e1ae9adc5a31b1d13fca39758131a757e1723068acd346ada600a"
-    
-    # https://github.com/closeio was verified as official when first introduced to the cask
-    url "https://github.com/closeio/closeio-desktop-releases/releases/download/v#{version}/Close-#{version}.zip"
-    appcast "https://github.com/closeio/closeio-desktop-releases/releases.atom"
-    name "Close CRM"
-    homepage "https://close.com/"
+  version "5.0.0"
+  sha256 "0b561615382e1ae9adc5a31b1d13fca39758131a757e1723068acd346ada600a"
   
-    # auto_updates true
-  
-    app "Close.app"
-  
+  # https://github.com/closeio was verified as official when first introduced to the cask
+  url "https://github.com/closeio/closeio-desktop-releases/releases/download/v#{version}/Close-#{version}.zip"
+  appcast "https://github.com/closeio/closeio-desktop-releases/releases.atom"
+  name "Close CRM"
+  homepage "https://close.com/"
+
+  # auto_updates true
+
+  app "Close.app"
 end
