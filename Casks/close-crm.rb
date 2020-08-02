189458935d4a4b235749d3b429b1bf252cf7413a
Daniel Lohrman <d@lhr.mn>
2020-08-02 17:03:58 +1000

    Update close-crm.rb

--- Casks/close-crm.rb
+++ Casks/close-crm.rb
@@ -1,7 +1,7 @@
 cask "close-crm" do
   version "5.0.0"
   sha256 "0b561615382e1ae9adc5a31b1d13fca39758131a757e1723068acd346ada600a"
-  
+
   # github.com/closeio was verified as official when first introduced to the cask
   url "https://github.com/closeio/closeio-desktop-releases/releases/download/v#{version}/Close-#{version}.zip"
   appcast "https://github.com/closeio/closeio-desktop-releases/releases.atom"
